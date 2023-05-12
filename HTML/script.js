9062151946993bb57bc1bf30e85a919528b8afd4
Mart5906 <131265980+Mart5906@users.noreply.github.com>
2023-05-12 10:01:02 +0200

    Add files via upload

---
+++ HTML/script.js
@@ -0,0 +1,12 @@
+'use strict';
+//Vanilla JS
+//Hello world
+console.log('hola buenas tardes');
+
+//Variables
+let hola = 'Tini';
+let buenas = 'Tini';
+let tardes = 'Tini';
+
+//Concatenación (+)
+console.log(hola + buenas + tardes);
